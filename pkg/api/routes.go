dda8e17fb0ee4d8c1747d1fdc8ea26c8d6b5db88
Gabriel Pereira Monte <gabrielpmonte@hotmail.com>
2023-04-21 10:43:54 -0300

    refactor(api): remove desnecessary middleware "Real IP"

--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -12,7 +12,6 @@ func GetRouter() *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
